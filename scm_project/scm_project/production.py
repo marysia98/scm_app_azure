d827272e280bbd2dbbdc997be30cb0d2d1c73478
marysia98 <maria_jarek@wp.pl>
2021-02-04 20:38:42 +0100

    add production.py for azure

---
+++ scm_project/scm_project/production.py
@@ -0,0 +1,26 @@
+from .settings import *
+import os
+
+# Configure the domain name using the environment variable
+# that Azure automatically creates for us.
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+
+MIDDLEWARE = [                                                                   
+    'django.middleware.security.SecurityMiddleware',                
+]
+
+
+# DBHOST is only the server name, not the full URL
+hostname = os.environ['DBHOST']
+
+# Configure Postgres database; the full username is username@servername,
+# which we construct using the DBHOST value.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DBNAME'],
+        'HOST': hostname + ".postgres.database.azure.com",
+        'USER': os.environ['DBUSER'] + "@" + hostname,
+        'PASSWORD': os.environ['DBPASS'] 
+    }
+}
