e090ef9dcb6bf8e77d6a02f4940ee603062d1f8a
marysia98 <maria_jarek@wp.pl>
2021-02-04 21:25:38 +0100

    change for proper module name in manage.py

--- scm_project/manage.py
+++ scm_project/manage.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == '__main__':
     # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
     # on Azure App Service and should use the production settings.
-    settings_module = "azuresite.production" if 'WEBSITE_HOSTNAME' in os.environ else 'azuresite.settings'
+    settings_module = "scm_project.production" if 'WEBSITE_HOSTNAME' in os.environ else 'scm_project.settings'
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
     try:
