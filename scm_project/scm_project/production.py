0b1828749a73c9a6b56d75208612d80acd1c3549
marysia98 <maria_jarek@wp.pl>
2021-02-04 22:16:23 +0100

    change middleware

--- scm_project/scm_project/production.py
+++ scm_project/scm_project/production.py
@@ -6,7 +6,13 @@ import os
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 MIDDLEWARE = [                                                                   
-    'django.middleware.security.SecurityMiddleware',                
+    'django.middleware.security.SecurityMiddleware',    
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',    
 ]
 
 
